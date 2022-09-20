b6a4159378aa582f270a8b05e2c15c28b2904e2f
miel3k <kuba.mielczarek96@icloud.com>
2022-09-20 20:59:07 +0200

    Remove interactor

--- app/src/main/java/com/miel3k/masteringandroidpaging3/room/viewmodel/RoomViewModel.kt
+++ app/src/main/java/com/miel3k/masteringandroidpaging3/room/viewmodel/RoomViewModel.kt
@@ -1,8 +1,14 @@
 package com.miel3k.masteringandroidpaging3.room.viewmodel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.*
+import com.miel3k.masteringandroidpaging3.data.pagingkey.PagingKeyDataSource
 import com.miel3k.masteringandroidpaging3.data.user.UserDataSource
+import com.miel3k.masteringandroidpaging3.data.user.model.User
 import com.miel3k.masteringandroidpaging3.di.UserDataModule
+import com.miel3k.masteringandroidpaging3.users.UsersPagingMediator
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
@@ -11,7 +17,25 @@ import javax.inject.Inject
  */
 @HiltViewModel
 class RoomViewModel @Inject constructor(
+    private val pagingKeyRepository: PagingKeyDataSource,
     @UserDataModule.RoomUserDataSource private val userRepository: UserDataSource
 ) : ViewModel() {
 
+    @OptIn(ExperimentalPagingApi::class)
+    val userPagingData: LiveData<PagingData<User>> by lazy {
+        val config = PagingConfig(
+            USERS_PAGE_SIZE,
+            initialLoadSize = USERS_PAGE_SIZE,
+            prefetchDistance = USERS_PAGE_SIZE / 2
+        )
+        val mediator = UsersPagingMediator(pagingKeyRepository, userRepository)
+        val factory = { userRepository.getUsersPagingSource() }
+        Pager(config, remoteMediator = mediator, pagingSourceFactory = factory)
+            .liveData
+            .cachedIn(viewModelScope)
+    }
+
+    private companion object {
+        const val USERS_PAGE_SIZE = 20
+    }
 }
